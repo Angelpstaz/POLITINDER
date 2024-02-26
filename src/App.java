eeaa7d2dfdb0e132dedfd38cdb7460433e869694
JuliaanCZ <juliancamacho897@gmail.com>
2024-02-25 21:28:49 -0500

    RelacionTipo

--- src/App.java
+++ src/App.java
@@ -1,12 +1,18 @@
 import DataAccessComponent.RegaloTipoDAO;
 import DataAccessComponent.DTO.RegaloTipoDTO;
+import DataAccessComponent.RelacionTipoDAO;
+import DataAccessComponent.DTO.RelacionTipoDTO;
 
 public class App {
     public static void main(String[] args) throws Exception {
         RegaloTipoDAO n = new RegaloTipoDAO();
         RegaloTipoDTO q = new RegaloTipoDTO();
+        RelacionTipoDAO rtdao = new RelacionTipoDAO();
+        RelacionTipoDTO rtdto = new RelacionTipoDTO();
         q = n.read(1);
+        rtdto = rtdao.read(1);
         System.out.println(q.getNombre());
+        System.out.println(rtdto.getNombre());
         
     }
 }
