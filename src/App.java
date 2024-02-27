bd9f11c446fd7a59a9f8dc3b1836a075086d43a5
Juan <jfchanatasig1405@gmail.com>
2024-02-26 19:50:28 -0500

    menu opciones

--- src/App.java
+++ src/App.java
@@ -1,12 +1,17 @@
 import DataAccessComponent.RegaloTipoDAO;
 import DataAccessComponent.DTO.RegaloTipoDTO;
+import UserInterface.FrameMenuPantallas;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        RegaloTipoDAO n = new RegaloTipoDAO();
-        RegaloTipoDTO q = new RegaloTipoDTO();
-        q = n.read(1);
-        System.out.println(q.getNombre());
+        // RegaloTipoDAO n = new RegaloTipoDAO();
+        // RegaloTipoDTO q = new RegaloTipoDTO();
+        // q = n.read(1);
+        // System.out.println(q.getNombre());
+        FrameMenuPantallas frameMenu = new FrameMenuPantallas();
+        frameMenu.frame.setVisible(true);
         
     }
+   
 }
+
